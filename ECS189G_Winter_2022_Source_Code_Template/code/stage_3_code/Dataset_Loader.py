72a03c83b0fd80e59bd8de3687942c0ed48060a0
jotyot <104177975+jotyot@users.noreply.github.com>
2024-02-13 20:41:25 -0800

    mnist done too

--- ECS189G_Winter_2022_Source_Code_Template/code/stage_3_code/Dataset_Loader.py
+++ ECS189G_Winter_2022_Source_Code_Template/code/stage_3_code/Dataset_Loader.py
@@ -7,33 +7,33 @@ class Dataset_Loader(dataset):
     dataset_source_folder_path = None
     dataset_source_file_name = None
     cmap = None
-    
+
     def __init__(self, dName=None, dDescription=None):
         super().__init__(dName, dDescription)
-    
+
     def load(self):
         f = open(self.dataset_source_folder_path + self.dataset_source_file_name, 'rb')
         data = pickle.load(f)
         f.close()
-        for instance in data['train']:
-            image_matrix = instance['image']
-            image_label = instance['label']
-            plt.imshow(image_matrix, cmap=self.cmap)
-            plt.show()
-            print(image_matrix)
-            print(image_label)
-            # remove the following "break" code if you would like to see more image in the training set
-            break
-            
-        for instance in data['test']:
-            image_matrix = instance['image']
-            image_label = instance['label']
-            plt.imshow(image_matrix, cmap=self.cmap)
-            plt.show()
-            print(image_matrix)
-            print(image_label)
-            # remove the following "break" code if you would like to see more image in the testing set
-            break
+        # for instance in data['train']:
+        #     image_matrix = instance['image']
+        #     image_label = instance['label']
+        #     plt.imshow(image_matrix, cmap=self.cmap)
+        #     plt.show()
+        #     print(image_matrix)
+        #     print(image_label)
+        #     # remove the following "break" code if you would like to see more image in the training set
+        #     break
+
+        # for instance in data['test']:
+        #     image_matrix = instance['image']
+        #     image_label = instance['label']
+        #     plt.imshow(image_matrix, cmap=self.cmap)
+        #     plt.show()
+        #     print(image_matrix)
+        #     print(image_label)
+        #     # remove the following "break" code if you would like to see more image in the testing set
+        #     break
 
         return {
             'train' : {
