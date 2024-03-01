b710c5592c1393240094b1c36d302295d662d921
jotyot <104177975+jotyot@users.noreply.github.com>
2024-02-29 20:07:50 -0800

    i saved the tokens

--- ECS189G_Winter_2022_Source_Code_Template/code/stage_4_code/Dataset_Loader.py
+++ ECS189G_Winter_2022_Source_Code_Template/code/stage_4_code/Dataset_Loader.py
@@ -1,58 +1,44 @@
-from io import TextIOWrapper
 from code.base_class.dataset import dataset
 import os
-import nltk
+import string
 from nltk import word_tokenize
-from concurrent.futures import ProcessPoolExecutor
+import pickle
 
 
 class Dataset_Loader(dataset):
-    train_data = None
-    test_data =None
-    train_labels = None
-    test_labels = None
-    test_dataset_source_folder_path = None
-    train_dataset_source_folder_path = None
-    dataset_source_file_name = None
-    cmap = None
+    dataset_source_folder_path = None
+    train_pos_source_file_name = None
+    train_neg_source_file_name = None
+    test_pos_source_file_name = None
+    test_neg_source_file_name = None
 
-    def __init__(self, dName=None, dDescription=None):
-        super().__init__(dName, dDescription)
-        self.data = []
-        self.labels = []
-
-    def load_data(self, directory, list: list):
-        for file in os.listdir(directory):
-            with open(os.path.join(directory, file), 'rt', encoding='utf-8') as f:
-                line = f.readline()
-                line = line.strip('\n')
-                tokens = word_tokenize(line)
-                list.append(tokens)
     def load(self):
         print("loading data...")
 
-        train_pos_path = (
-            self.dataset_source_folder_path + self.traindata_pos_source_file_name
-        )
-        train_neg_path = (
-            self.dataset_source_folder_path + self.traindata_neg_source_file_name
-        )
-        test_pos_path = (
-            self.dataset_source_folder_path + self.testdata_pos_source_file_name
-        )
-        test_neg_path = (
-            self.dataset_source_folder_path + self.testdata_neg_source_file_name
-        )
-
         neg_train = []
         pos_train = []
-        self.load_data(train_neg_path, neg_train)
-        self.load_data(train_pos_path, pos_train)
-
         neg_test = []
         pos_test = []
-        self.load_data(test_neg_path, neg_test)
-        self.load_data(train_pos_path, pos_test)
+
+        with open(
+            self.dataset_source_folder_path + self.train_pos_source_file_name, "rb"
+        ) as file:
+            pos_train = pickle.load(file)
+
+        with open(
+            self.dataset_source_folder_path + self.train_neg_source_file_name, "rb"
+        ) as file:
+            neg_train = pickle.load(file)
+
+        with open(
+            self.dataset_source_folder_path + self.test_pos_source_file_name, "rb"
+        ) as file:
+            pos_test = pickle.load(file)
+
+        with open(
+            self.dataset_source_folder_path + self.test_neg_source_file_name, "rb"
+        ) as file:
+            neg_test = pickle.load(file)
 
         data = {
             "train": {
