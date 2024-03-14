d64c2871815995ead0ba1841348f00ea5948f70c
adbayani <adbayani@ucdavis.edu>
2024-03-14 02:53:16 -0700

    mwefw

---
+++ ECS189G_Winter_2022_Source_Code_Template/code/stage_5_code/Method_GCN.py
@@ -0,0 +1,158 @@
+"""
+Concrete MethodModule class for a specific learning MethodModule
+"""
+
+# Copyright (c) 2017-Current Jiawei Zhang <jiawei@ifmlab.org>
+# License: TBD
+
+from code.base_class.method import method
+from torch.utils.data import TensorDataset, DataLoader
+from code.stage_1_code.Evaluate_Accuracy import Evaluate_Accuracy
+import torch
+from torch import nn
+import numpy as np
+import time
+from torch_geometric.nn import GCNConv
+class Method_GCN(method, nn.Module):
+    data = None
+    glove_embeddings = None
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    batch_size = 5000
+    max_epoch = 250
+    learning_rate = 1e-3
+
+    max_review_length = 120
+    hidden_size = 35
+    num_layers = 5
+    dropout_rate = 0  # 0.1
+    weight_decay = 0  # 8e-4
+
+    # terminate training if it gets this accurate cuz it might be overfitting
+    termination_acc = 0.88
+    loss_history = []
+
+    def __init__(self, mName, mDescription):
+        method.__init__(self, mName, mDescription)
+        nn.Module.__init__(self)
+        self.rnn = nn.LSTM(
+            50,
+            self.hidden_size,
+            self.num_layers,
+            batch_first=True,
+            dropout=self.dropout_rate,
+        )
+        self.fc = nn.Linear(self.hidden_size, 2)
+        self.glove_embeddings = self.load_glove("data/stage_4_data/glove.6B.50d.txt")
+
+    # it defines the forward propagation function for input x
+    # this function will calculate the output layer by layer
+
+    def forward(self, X):
+        out, _ = self.rnn(X)
+        out = self.fc(out[:, -1, :])
+        return out
+
+    def load_glove(self, glove_file):
+        index = {}
+        with open(glove_file, 'r', encoding='utf-8') as f:
+            for line in f:
+                values = line.split()
+                word = values[0]
+                coefficients = np.asarray(values[1:], dtype='float32')
+                index[word] = coefficients
+        return index
+    # Matches words in the vectors to glove embeddings
+    def data_preprocess(self, X):
+        embedding_length = 50
+        tensor = np.zeros((len(X), self.max_review_length, embedding_length))
+
+        for i, review in enumerate(X):
+            for j, word in enumerate(review):
+                if j == self.max_review_length:
+                    break
+                if word not in self.glove_embeddings:
+                    continue
+                embedding = self.glove_embeddings.get(word)
+
+                tensor[i, j, :] = embedding
+
+        return tensor
+
+    def train(self, X, y):
+        # check here for the torch.optim doc: https://pytorch.org/docs/stable/optim.html
+        optimizer = torch.optim.Adam(
+            self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay
+        )
+        # check here for the nn.CrossEntropyLoss doc: https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html
+        loss_function = nn.CrossEntropyLoss()
+        # for training accuracy investigation purpose
+        accuracy_evaluator = Evaluate_Accuracy("training evaluator", "")
+        dataset = TensorDataset(
+            torch.tensor(self.data_preprocess(X), device=self.device, dtype=torch.float32),
+            torch.tensor(np.array(y), device=self.device, dtype=torch.long),
+        )
+        train_loader = DataLoader(dataset, batch_size=self.batch_size, shuffle=True)
+
+        for epoch in range(self.max_epoch):
+            running_loss = 0.0
+            for inputs, y_true in train_loader:
+                optimizer.zero_grad()
+                y_pred = self.forward(inputs)
+                train_loss = loss_function(y_pred, y_true)
+                train_loss.backward()
+                optimizer.step()
+                running_loss += train_loss.item()
+
+            self.loss_history.append(running_loss / len(train_loader))
+            if epoch % 5 == 0 or epoch == self.max_epoch - 1:
+                accuracy_evaluator.data = {
+                    "true_y": y_true.cpu(),
+                    "pred_y": y_pred.cpu().max(1)[1],
+                }
+                accuracy = accuracy_evaluator.evaluate()
+                print(
+                    "Epoch:",
+                    epoch,
+                    "Accuracy:",
+                    accuracy,
+                    "Loss:",
+                    running_loss / len(train_loader),
+                )
+            if accuracy > self.termination_acc:
+                break
+
+    def test(self, X):
+        test_loader = DataLoader(torch.tensor(self.data_preprocess(X), device=self.device, dtype=torch.float32), batch_size=250)
+
+        y_pred_list = []
+
+        for inputs in test_loader:
+            outputs = self.forward(inputs)
+            y_pred_list.append(outputs.max(1)[1])
+
+        y_pred = torch.cat(y_pred_list)
+
+        return y_pred
+
+    def run(self):
+        start = time.perf_counter()
+        print("method running...")
+        print("--start training...")
+        self.train(self.data["train"]["X"], self.data["train"]["y"])
+        print("--start testing...")
+        pred_y_train = self.test(self.data["train"]["X"])
+        pred_y_test = self.test(self.data["test"]["X"])
+        end = time.perf_counter()
+        print((end - start)/60, " minutes elapsed")
+        return (
+            {
+                "pred_y": pred_y_train.cpu(),
+                "true_y": self.data["train"]["y"],
+            },
+            {
+                "pred_y": pred_y_test.cpu(),
+                "true_y": self.data["test"]["y"],
+            },
+            self.loss_history,
+        )
---
+++ ECS189G_Winter_2022_Source_Code_Template/script/stage_5_script/script_cora.py
@@ -0,0 +1,65 @@
+from code.stage_5_code.Dataset_Loader_Node_Classification import Dataset_Loader
+from code.stage_5_code.Method_GCN import Method_GCN
+from code.stage_2_code.Result_Saver import Result_Saver
+from code.stage_2_code.Setting_Train_Test import Setting_Train_Test
+from code.stage_2_code.Evaluate_Accuracy import Evaluate_Accuracy
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
+# run with
+# python3 -m script.stage_4_script.script_classifier
+
+# ---- recurring neural net script ----
+if 1:
+    # ---- parameter section -------------------------------
+    np.random.seed(2)
+    torch.manual_seed(2)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    # ------------------------------------------------------
+
+    # ---- objection initialization setction ---------------
+    data_obj = Dataset_Loader("train", "")
+    data_obj.dataset_source_folder_path = (
+        "data/stage_5_data/cora"
+    )
+    method_obj = Method_GCN("graph convolutional network", "").to(device)
+
+    result_obj = Result_Saver("saver", "")
+    result_obj.result_destination_folder_path = "result/stage_5_result/cora_"
+    result_obj.result_destination_file_name = "prediction_result"
+
+    setting_obj = Setting_Train_Test("train test sets", "")
+
+    evaluate_obj = Evaluate_Accuracy("accuracy", "")
+    # ------------------------------------------------------
+
+    # ---- running section ---------------------------------
+    print("************ Start ************")
+    setting_obj.prepare(data_obj, method_obj, result_obj, evaluate_obj)
+    setting_obj.print_setup_summary()
+    (
+        train_evaluations,
+        test_evaluations,
+        accuracy_history,
+    ) = setting_obj.load_run_save_evaluate()
+    accuracy, precision, recall, fscore = train_evaluations
+    print("************ Training Set Performance ************")
+    print("RNN Accuracy: " + str(accuracy))
+    print("RNN Precision: " + str(precision))
+    print("RNN Recall: " + str(recall))
+    print("RNN FScore: " + str(fscore))
+    accuracy, precision, recall, fscore = test_evaluations
+    print("************ Testing Set Performance ************")
+    print("RNN Accuracy: " + str(accuracy))
+    print("RNN Precision: " + str(precision))
+    print("RNN Recall: " + str(recall))
+    print("RNN FScore: " + str(fscore))
+    print("************ Finish ************")
+
+    plt.plot(range(len(accuracy_history)), accuracy_history)
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.title("Loss over Epochs")
+    plt.savefig(result_obj.result_destination_folder_path + "learning_graph.png")
+    # ------------------------------------------------------
